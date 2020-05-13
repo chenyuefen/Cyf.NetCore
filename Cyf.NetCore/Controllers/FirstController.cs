7c9e5f61ff56e69a5409b060fca4f3e98563e725
越锋 陈 <3510329812@qq.com>
2020-05-13 15:35:39 +0800

    [First]页面运行时修改自动重新编译

--- Cyf.NetCore/Controllers/FirstController.cs
+++ Cyf.NetCore/Controllers/FirstController.cs
@@ -6,6 +6,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Cyf.NetCore.Controllers
 {
+    /// <summary>
+    /// *********** 运行时修改自动重新编译 ***********
+    /// -> nuget增加 Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation 引用
+    /// -> Startup[ConfigureServices] 增加 .AddRazorRuntimeCompilation();
+    /// </summary>
     public class FirstController : Controller
     {
         public IActionResult Index()
