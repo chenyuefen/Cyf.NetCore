2b9b134bdfbf2863226c30081e999e6a57cc80a0
越锋 陈 <3510329812@qq.com>
2020-05-13 15:24:30 +0800

    添加项目文件。

---
+++ Cyf.NetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cyf.NetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
