d63ec353111bb8ed5842a828142efb86e041f61d
越锋 陈 <3510329812@qq.com>
2020-05-18 11:49:27 +0800

    1

--- Cyf.EF.Business/CompanyService.cs
+++ Cyf.EF.Business/CompanyService.cs
@@ -1,10 +1,5 @@
 ﻿using Cyf.EntityFramework.Interface;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cyf.EntityFramework.Business
 {
