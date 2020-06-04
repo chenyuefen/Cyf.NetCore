c6c11c3d47b9d68602ce24f577391b0a6423c74f
越锋 陈 <3510329812@qq.com>
2020-06-04 11:05:05 +0800

    [微服务核心]IdentityServer4将资源数据,用户数据，身份数据全部进行放在数据库进行存储。通过数据库进行持久化操作

--- Cyf.AuthorizationServer/Startup.cs
+++ Cyf.AuthorizationServer/Startup.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Cyf.AuthorizationServer.Models;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,26 +30,44 @@ namespace Cyf.AuthorizationServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //InMemoryConfiguration.Configuration = this.Configuration;
-            //services.AddIdentityServer()
-            //    .AddDeveloperSigningCredential()//临时证书，AddSigningCredential正式证书
-            //    .AddTestUsers(InMemoryConfiguration.GetUsers().ToList())//用户
-            //    .AddInMemoryClients(InMemoryConfiguration.GetClients())//客户端
-            //    .AddInMemoryApiResources(InMemoryConfiguration.GetApiResources());//那些接口
-
             // 1、ioc容器中添加IdentityServer4
+            //services.AddIdentityServer()
+            //    .AddDeveloperSigningCredential()// 1、用户登录配置
+            //    .AddInMemoryApiResources(Config.GetApiResources()) // 2、存储Api资源
+            //    .AddInMemoryClients(Config.GetClients()) // 3、存储客户端(模式)
+            //    .AddTestUsers(Config.GetUsers())// 4、添加登录用户(模式)
+            //    .AddInMemoryIdentityResources(Config.Ids); // 4、使用openid模式	;
+
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+            var connectionString = Configuration.GetConnectionString("CyfMSSQLConnection");
             services.AddIdentityServer()
-                .AddDeveloperSigningCredential()// 1、用户登录配置
-                .AddInMemoryApiResources(Config.GetApiResources()) // 2、存储Api资源
-                .AddInMemoryClients(Config.GetClients()) // 3、存储客户端(模式)
-                .AddTestUsers(Config.GetUsers())// 4、添加登录用户(模式)
-                .AddInMemoryIdentityResources(Config.Ids); // 4、使用openid模式	;
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                    {
+                        builder.UseSqlServer(connectionString, options =>
+                             options.MigrationsAssembly(migrationsAssembly));
+                    };
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                    {
+                        builder.UseSqlServer(connectionString, options =>
+                  options.MigrationsAssembly(migrationsAssembly));
+                    };
+                })
+                .AddTestUsers(Config.GetUsers())
+                .AddDeveloperSigningCredential();
+
             services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //InitializeDatabase(app);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -74,6 +96,43 @@ namespace Cyf.AuthorizationServer
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        // 1、将config中数据存储起来
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetService<PersistedGrantDbContext>().Database.Migrate();
+                var context = serviceScope.ServiceProvider.GetService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.Ids)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.GetApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
