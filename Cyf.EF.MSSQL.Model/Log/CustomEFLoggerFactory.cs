904b8e11f14e3081f0fb85384f49cbfcc16abc7a
越锋 陈 <3510329812@qq.com>
2020-06-05 11:52:09 +0800

    添加忽略文件

---
+++ Cyf.EF.MSSQL.Model/Log/CustomEFLoggerFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using System.Text;
+
+namespace Cyf.EF.MSSQL.Model.Log
+{
+    public class CustomEFLoggerFactory : ILoggerFactory
+    {
+        public void AddProvider(ILoggerProvider provider)
+        {
+
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new CustomEFLogger(categoryName);
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+
+}
