955c8c7fd96d9ef8f16a73ec493aab9d3758a95b
越锋 陈 <3510329812@qq.com>
2020-07-02 16:53:08 +0800

    [Redis]增加ServiceStack.Redis封装

---
+++ Helpers/Helpers.Redis/Interface/IRedisHashService.cs
@@ -0,0 +1,87 @@
+﻿using Zhaoxi.AspnetCore.Cache.Redis.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zhaoxi.AspnetCore.Cache.Redis.Interface
+{
+    /// <summary>
+    ///
+    /// </summary>
+    interface IRedisHashService 
+    {
+        #region 添加
+        /// <summary>
+        /// 向hashid集合中添加key/value
+        /// </summary>       
+        bool SetEntryInHash(string hashid, string key, string value);
+
+        /// <summary>
+        /// 如果hashid集合中存在key/value则不添加返回false，
+        /// 如果不存在在添加key/value,返回true
+        /// 
+        /// //
+        /// </summary>
+        bool SetEntryInHashIfNotExists(string hashid, string key, string value);
+        
+        /// <summary>
+        /// 存储对象T t到hash集合中
+        /// 需要包含Id，然后用Id获取
+        /// 存入的对象必须有一个Id
+        /// </summary>
+        void StoreAsHash<T>(T t);
+        #endregion
+
+        #region 获取
+        /// <summary>
+        /// 获取对象T中ID为id的数据。
+        /// </summary>
+        T GetFromHash<T>(object id);
+        /// <summary>
+        /// 获取所有hashid数据集的key/value数据集合
+        /// </summary>
+        Dictionary<string, string> GetAllEntriesFromHash(string hashid);
+
+        /// <summary>
+        /// 获取hashid数据集中的数据总数
+        /// </summary>
+        long GetHashCount(string hashid);
+        /// <summary>
+        /// 获取hashid数据集中所有key的集合
+        /// </summary>
+        List<string> GetHashKeys(string hashid);
+        /// <summary>
+        /// 获取hashid数据集中的所有value集合
+        /// </summary>
+        List<string> GetHashValues(string hashid);
+        /// <summary>
+        /// 获取hashid数据集中，key的value数据
+        /// </summary>
+        string GetValueFromHash(string hashid, string key);
+        /// <summary>
+        /// 获取hashid数据集中，多个keys的value集合
+        /// </summary>
+        List<string> GetValuesFromHash(string hashid, string[] keys);
+        #endregion
+
+        #region 删除
+        /// <summary>
+        /// 删除hashid数据集中的key数据
+        /// </summary>
+        bool RemoveEntryFromHash(string hashid, string key);
+        #endregion
+
+        #region 其它
+        /// <summary>
+        /// 判断hashid数据集中是否存在key的数据
+        /// </summary>
+        bool HashContainsEntry(string hashid, string key);
+        /// <summary>
+        /// 给hashid数据集key的value加countby，返回相加后的数据
+        /// </summary>
+        double IncrementValueInHash(string hashid, string key, double countBy);
+        #endregion
+    }
+}
---
+++ Helpers/Zhaoxi.AspnetCore.Cache.Redis/Interface/RedisBase.cs
@@ -0,0 +1,131 @@
+﻿ 
+using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zhaoxi.AspnetCore.Cache.Redis.Init;
+
+namespace Zhaoxi.AspnetCore.Cache.Redis.Interface
+{
+    /// <summary>
+    /// RedisBase类，是redis操作的基类，继承自IDisposable接口，主要用于释放内存
+    /// </summary>
+    public abstract class RedisBase : IDisposable
+    {
+        /// <summary>
+        /// Redis连接；
+        /// </summary>
+        public IRedisClient iClient { get; private set; }
+        /// <summary>
+        /// 构造时完成链接的打开
+        /// </summary>
+        public RedisBase()
+        {
+            iClient = RedisManager.GetClient();
+        }
+
+        //public static IRedisClient iClient { get; private set; }
+        //static RedisBase()
+        //{
+        //    iClient = RedisManager.GetClient();
+        //}
+
+
+        private bool _disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this._disposed)
+            {
+                if (disposing)
+                {
+                    iClient.Dispose();
+                    iClient = null;
+                }
+            }
+            this._disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Transcation()
+        {
+            using (IRedisTransaction irt = this.iClient.CreateTransaction())
+            {
+                try
+                {
+                    irt.QueueCommand(r => r.Set("key", 20));
+                    irt.QueueCommand(r => r.Increment("key", 1));
+                    irt.Commit(); // 提交事务
+                }
+                catch (Exception ex)
+                {
+                    irt.Rollback();
+                    throw ex;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 清除全部数据 请小心
+        /// </summary>
+        public virtual void FlushAll()
+        {
+            iClient.FlushAll();
+        }
+
+        /// <summary>
+        /// 保存数据DB文件到硬盘
+        /// </summary>
+        public void Save()
+        {
+            iClient.Save();//阻塞式save
+        }
+
+        /// <summary>
+        /// 异步保存数据DB文件到硬盘
+        /// </summary>
+        public void SaveAsync()
+        {
+            iClient.SaveAsync();//异步save
+        }
+
+        public bool Lock(string key, int expirySeconds = 5, double waitSeconds = 0)
+        {
+            int waitIntervalMs = 50;//间隔等待时长 毫秒 合理配置一下 应该也会影响性能 间隔时间太长肯定是不行的
+            string lockKey = "lock_key:" + key;
+
+            DateTime begin = DateTime.Now;
+            while (true)
+            {
+                if (((RedisClient)iClient).SetNX(lockKey, new byte[] { 1 }) == 1)
+                {
+                    ((RedisClient)iClient).Expire(lockKey, expirySeconds);
+                    return true;
+                }
+
+                //不等待锁则返回
+                if (waitSeconds <= 0)
+                    break;
+
+                if ((DateTime.Now - begin).TotalSeconds >= waitSeconds)//等待超时
+                    break;
+
+                System.Threading.Thread.Sleep(waitIntervalMs);
+            }
+            return false;
+        }
+
+        public long UnLock(string key)
+        {
+
+            string lockKey = "lock_key:" + key;
+            return ((RedisClient)iClient).Del(lockKey);
+        }
+    }
+}
